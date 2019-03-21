a4cc39f1eaf6b92bb12b8f7fe99177c61b95a578
Farshad <farshadcp@gmail.com>
2019-03-21 18:49:29 +0900

    Working copy - no visual design

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 "use strict";
 
+const MIN_DOT_DIAMETER = 10;
+const MAX_DOT_DIAMETER = 100;
 const winX = window.outerWidth;
 const winY = window.outerHeight;
 const canvas = document.getElementById("canvas");
 const c = canvas.getContext("2d");
 let dotArr = [];
-
-const MIN_DOT_RADIUS = 10;
-const MAX_DOT_RADIUS = 100;
+let isPlay = false;
+let mouseX = 0;
+let mouseY = 0;
+let score = 0;
 
 canvas.width = winX;
 canvas.height = winY;
@@ -15,9 +18,10 @@ canvas.height = winY;
 class Dot {
   constructor() {
     this.color = 'blue';
-    this.radius = getRndInteger(1, 10) * MIN_DOT_RADIUS;
-    this.x = getRndInteger(MAX_DOT_RADIUS, winX - MAX_DOT_RADIUS); // returns a random integer between 100 and winX - 100
-    this.y = 0;
+    this.radius = getRndInteger(1, 10) * MIN_DOT_DIAMETER / 2;
+    this.x = getRndInteger(MAX_DOT_DIAMETER, winX - MAX_DOT_DIAMETER); // returns a random integer between 100 and (winX - 100)
+    this.y = -this.radius;
+    this.points = (MAX_DOT_DIAMETER - this.radius * 2) / 10 + 1; // Points inversely proportional to radius
   }
 
   update() {
@@ -30,28 +34,61 @@ class Dot {
 
 function start() {
   document.getElementById('startBtn').style.display = 'none';
+  isPlay = true;
   dotArr.push(new Dot())
   animate();
-  setInterval(function() {
-    // Only 2 dots at a time on the screen
-    if (dotArr.length > 1) {
-      dotArr.splice(0, 1);
+
+  // Push dot every second
+  setInterval(function () {
+    if (isPlay) {
+      dotArr.push(new Dot());
     }
-    dotArr.push(new Dot());
   }, 1000);
 }
 
+window.addEventListener("click", function () {
+  mouseX = event.clientX;
+  mouseY = event.clientY;
+});
+
 function animate() {
-  requestAnimationFrame(animate);
+  if (!isPlay) {
+    cancelAnimationFrame(animate); // Cancel animation on game pause 
+    return;
+  }
+
+  requestAnimationFrame(animate); // Continue animation if game is on.
   c.clearRect(0, 0, winX, winY);
-  // Max array length will be 2 
   dotArr.forEach(each => {
     each.update();
-    each.y += 10;
+    // Speed range is 10px ~ 100px per second based on the configuration.
+    // requestAnimationFrame repaint every 1/60 seconds. So (y += 1 / 60 * <10 ~ 100>) adds (10 ~ 100) every second
+    each.y += 1 / 6;
+
+    // Logic to find clicked dot
+    if (mouseX > each.x - each.radius &&
+      mouseX < each.x + each.radius &&
+      mouseY > each.y - each.radius &&
+      mouseY < each.y + each.radius) {
+
+      each.radius = 0; // Hide clicked dot
+      score += each.points; // Increment score
+      document.getElementById('score').innerHTML = score;
+
+      // Reset clicked coordinates. Otherwise future dots of same coordinates will be considered as clicked
+      mouseX = 0;
+      mouseY = 0;
+    }
   });
 }
 
+function controlAnimation() {
+  isPlay = !isPlay; // Toggle game status
+  animate(); // Handle animations based on game status
+}
+
+// Utility method to find random numbers
 function getRndInteger(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) ) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
